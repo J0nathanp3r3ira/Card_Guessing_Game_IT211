97990923198824790c83715bc817af307184f325
J0nathanp3r3ira <63617418+J0nathanp3r3ira@users.noreply.github.com>
2020-08-09 22:56:43 -0700

    Create cardGame.java

---
+++ cardGame.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class cardGame {
+	public static deckOfCards deck;
+
+	public static void main(String[] args) throws IOException {
+		deck = new deckOfCards();
+		deck.shuffle();
+		new cardGUI();
+	}
